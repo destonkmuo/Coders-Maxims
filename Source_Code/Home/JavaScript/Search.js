b2a7f5f0ecfdd51bacc5d49e11ec6f4c61042b89
destonkmuo <110706572+destonkmuo@users.noreply.github.com>
2022-08-21 22:10:29 -0500

    More Advanced Search Features (Not Done)

--- Source_Code/Home/JavaScript/Search.js
+++ Source_Code/Home/JavaScript/Search.js
@@ -1,7 +1,27 @@
+var results = [];
+
 function Search() {
-    var modules = JSON.parse(Modules);
-    var input;
-    input = document.getElementById("input-search");
-    console.log(input.value);
-    console.log(modules[0].name);
-}
+    var input = document.getElementById("input-search").value.toLowerCase();
+    
+    fetch("../Metadata/Modules.json")
+        .then(response => response.json())
+        .then(modules => {
+            for (let i = 0; i < modules.length; i++) {
+                if(results.includes(modules[i].url) && results.length > 0) { return; } else { // Guard Clause for Overlapping Urls
+                if (input.includes(modules[i].name.toLowerCase())) {
+                    console.log("found... appending url");
+                    results.push(modules[i].url);
+                } else if (modules[i].keyTerms.includes(input)) {
+                    console.log("found... appending url");
+                    results.push(modules[i].url);
+                    console.log(results);
+                }
+                if(!results.includes(modules[i].url)) {
+                    results = [];
+                }
+                console.log(results);
+            }
+                //If Input is a Beyond Percentage Threshold then Instantiate URL as well?
+        }
+        })
+    }
