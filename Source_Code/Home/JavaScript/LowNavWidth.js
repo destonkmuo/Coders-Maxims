2019f3222ed600d23f0cd473990b766ba6cbc50f
destonkmuo <110706572+destonkmuo@users.noreply.github.com>
2022-08-22 21:15:33 -0500

    Progression On Search function

--- Source_Code/Home/JavaScript/LowNavWidth.js
+++ Source_Code/Home/JavaScript/LowNavWidth.js
@@ -1,12 +1,12 @@
 var navList = document.getElementsByClassName('list-ns');
 var button = document.getElementById('low-nav-width-btn');
-var dropDown = document.getElementsByClassName('low-nav-width-drop-down')
+var dropDown = document.getElementsByClassName('low-nav-width-drop-down');
 var width, height;
 
 window.onresize = window.onload = function() {    
     width = window.innerWidth;
     height = window.innerHeight;
-    if(width < 1225) {
+    if(width < 1255) {
         for (let i = 1; i < navList.length - 1; i++) {
         navList[i].style.display = "none";
         button.style.display = "block";
--- Source_Code/Home/JavaScript/Search.js
+++ Source_Code/Home/JavaScript/Search.js
@@ -1,27 +1,42 @@
+const searchResultsContent = document.getElementById('results-content');
+const searchButton = document.getElementById('search-button');
 var results = [];
 
 function Search() {
     var input = document.getElementById("input-search").value.toLowerCase();
-    
     fetch("../Metadata/Modules.json")
         .then(response => response.json())
         .then(modules => {
             for (let i = 0; i < modules.length; i++) {
-                if(results.includes(modules[i].url) && results.length > 0) { return; } else { // Guard Clause for Overlapping Urls
-                if (input.includes(modules[i].name.toLowerCase())) {
-                    console.log("found... appending url");
-                    results.push(modules[i].url);
-                } else if (modules[i].keyTerms.includes(input)) {
-                    console.log("found... appending url");
-                    results.push(modules[i].url);
-                    console.log(results);
-                }
-                if(!results.includes(modules[i].url)) {
-                    results = [];
-                }
-                console.log(results);
+                if (results.includes(modules[i].url) && modules[i].keyTerms.includes(input)) { return; } else { 
+                    // Guard Clause for Overlapping Urls
+                    if (modules[i].keyTerms.includes(input)) {
+                        console.log("found... appending url");
+                        results.push(modules[i].url);
+                    } else { results = []; }
+                } 
             }
-                //If Input is a Beyond Percentage Threshold then Instantiate URL as well?
-        }
+
+            
+            if (results.length > 0) {
+                searchResultsContent.style.visibility = "visible";
+                searchResultsContent.style.pointerEvents = "all";
+            } else {
+                searchResultsContent.style.visibility = "hidden";
+                searchResultsContent.style.pointerEvents = "none";
+            }
+            searchButton.onmousedown = function() {
+                console.log("1");
+                if (results.length > 0) {
+                searchResultsContent.style.visibility = "visible";
+                searchResultsContent.style.pointerEvents = "all";
+                }
+            }
+            searchButton.onmouseleave = function() { // Eventually use a class for the list box so it wont pp poo poo
+                console.log("2");
+                searchResultsContent.style.visibility = "hidden";
+                searchResultsContent.style.pointerEvents = "none";
+            }
+            //Create a list that you append the top 5 search values to, that are hyper text anchors
         })
-    }
+}
