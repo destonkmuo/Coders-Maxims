57bb8c5b8b313510d1f481567c4415c921887f1a
destonkmuo <110706572+destonkmuo@users.noreply.github.com>
2022-08-23 22:23:26 -0500

    FInished Main Functionality Of Search Bar

--- Source_Code/Home/JavaScript/Search.js
+++ Source_Code/Home/JavaScript/Search.js
@@ -4,39 +4,57 @@ var results = [];
 
 function Search() {
     var input = document.getElementById("input-search").value.toLowerCase();
+    console.log(input);
     fetch("../Metadata/Modules.json")
         .then(response => response.json())
         .then(modules => {
-            for (let i = 0; i < modules.length; i++) {
-                if (results.includes(modules[i].url) && modules[i].keyTerms.includes(input)) { return; } else { 
-                    // Guard Clause for Overlapping Urls
-                    if (modules[i].keyTerms.includes(input)) {
-                        console.log("found... appending url");
-                        results.push(modules[i].url);
-                    } else { results = []; }
-                } 
-            }
+            for (let modulesIndex = 0; modulesIndex < modules.length; modulesIndex++) {
+                if (results.includes(modules[modulesIndex].url)) {
+                    while (searchResultsContent.firstChild) {searchResultsContent.removeChild(searchResultsContent.lastChild) }
+                    console.log("Results Already Contains User Input");
+                    results = [];
+                    return;
+                } else if (modules[modulesIndex].keyTerms.includes(input)) {
+                    console.log("Found... Appending Url");
+                    results.push(modules[modulesIndex].url);
+                }
 
-            
-            if (results.length > 0) {
-                searchResultsContent.style.visibility = "visible";
-                searchResultsContent.style.pointerEvents = "all";
-            } else {
-                searchResultsContent.style.visibility = "hidden";
-                searchResultsContent.style.pointerEvents = "none";
-            }
-            searchButton.onmousedown = function() {
-                console.log("1");
-                if (results.length > 0) {
-                searchResultsContent.style.visibility = "visible";
-                searchResultsContent.style.pointerEvents = "all";
+                if (results.includes(modules[modulesIndex].url)) {
+                    var newUrl = document.createElement("a");
+                    newUrl.innerHTML = modules[modulesIndex].name;
+                    newUrl.href = modules[modulesIndex].url;
+                    newUrl.className = "results-lists";
+                    document.body.appendChild(newUrl);
+                    searchResultsContent.appendChild(newUrl);
                 }
             }
-            searchButton.onmouseleave = function() { // Eventually use a class for the list box so it wont pp poo poo
-                console.log("2");
-                searchResultsContent.style.visibility = "hidden";
-                searchResultsContent.style.pointerEvents = "none";
+
+            console.log(results);
+
+            function visibility(boolean) {
+                if (boolean) {
+                    searchResultsContent.style.visibility = "visible";
+                    searchResultsContent.style.pointerEvents = "all";
+                } else {
+                    searchResultsContent.style.visibility = "hidden";
+                    searchResultsContent.style.pointerEvents = "none";
+                }
             }
+            if (results.length > 0) {
+                visibility(true)
+            } else {
+                visibility(false)
+            }
+
+            searchButton.onmousedown = function() {
+                if (results.length > 0) {
+                    visibility(true)
+                }
+            }
+            searchButton.onmouseleave = function() {
+                visibility(false)
+            }
+
             //Create a list that you append the top 5 search values to, that are hyper text anchors
         })
 }
