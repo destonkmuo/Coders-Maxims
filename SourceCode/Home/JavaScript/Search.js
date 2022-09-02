07edd884c4d29638812fa7cba71fa512973d5091
destonkmuo <110706572+destonkmuo@users.noreply.github.com>
2022-09-02 14:43:34 -0500

    Rename of Source Code and Small Bug Fixes

--- SourceCode/Home/JavaScript/Search.js
+++ SourceCode/Home/JavaScript/Search.js
@@ -2,6 +2,8 @@ const searchResultsContent = document.getElementById('results-content');
 const searchButton = document.getElementById('search-button');
 var results = [];
 
+var devLog = false;
+
 function resetResults() {
     while (searchResultsContent.firstChild) {searchResultsContent.removeChild(searchResultsContent.lastChild) }
     results = [];
@@ -14,14 +16,14 @@ function Search() {
         .then(modules => {
             for (let modulesIndex = 0; modulesIndex < modules.length; modulesIndex++) {
                 if (results.includes(modules[modulesIndex].href)) {
-                    console.log("Results Already Contains User Input... Resetting: "+ results);
+                    if(devLog) { console.error(`Results Already Contains User Input... Resetting: ${results}`) }
                     resetResults();
                     return;
                 } else if(modules[modulesIndex].keyTerms.filter(keyTerms => keyTerms.startsWith(input)).length > 0 && input.length > 0){
-                    console.log(`Found ${modules[modulesIndex].name}... Appending Potential href`);
+                    if(devLog) { console.log(`Found ${modules[modulesIndex].name}... Appending Potential href`) }
                     results.push(modules[modulesIndex].href);
-                } else if(input.includes(modules[modulesIndex].name.toLowerCase())){
-                    console.log(`Found ${modules[modulesIndex].name}... Appending Potential href`);
+                } else if((modules[modulesIndex].name.replace(' ','').toLowerCase().startsWith(input.replace(' ',''))) && input.length > 0){
+                    if(devLog) { console.log(`Found ${modules[modulesIndex].name}... Appending Potential href`) }
                     results.push(modules[modulesIndex].href);
                 }
                 
@@ -44,25 +46,40 @@ function Search() {
                     searchResultsContent.style.pointerEvents = "none";
                 }
             }
-            if (results.length > 0) { visibility(true) } else { visibility(false) }
 
+            if (results.length > 0) { visibility(true) } else { visibility(false) }
             searchButton.onmousedown = function() { if (results.length > 0) { visibility(true) }}
             searchButton.onmouseleave = function() { visibility(false) }
         })
-        console.log("Search Request Complete: " + results);
-        resetResults();
+    if(devLog) { console.log(`Search Request Complete: ${results}`); }
+    resetResults();
 }
 
-/*       var timesRun = 0;
-        var interval = setInterval(function(){
-        timesRun += 1;
-        if(timesRun === 60){
-            clearInterval(interval);
-        }
-        var arrOfwords = [["H","e","l","l","o"," ","W","o","r","l","d"]]
-        document.getElementById("input-search").placeholder = "";
-        }, 2000);
-        
-        if you're feeling a bit chunky :D
 
-        */
+/*document.addEventListener("DOMContentLoaded", function(event) { 
+var time = 0;
+var currentString = [];
+var timeSpeed = 400;
+var arrOfwords = [["H","e","l","l","o"," ","W","o","r","l","d"],[""]]
+var increment = true;
+
+var interval = setInterval(function(){
+    if(time == arrOfwords[0].length && increment == true){
+        increment = false;
+        console.log(increment);
+        time = 1;
+        timeSpeed = 200;
+        currentString.pop();
+        document.getElementById("input-search").placeholder = `Search... ${currentString.join("")}`;
+        if(currentString.length == 0) {
+            console.log(currentString.length);
+            increment = true;
+        }
+    }
+    currentString.push(arrOfwords[0][time]);
+    document.getElementById("input-search").placeholder = `Search... ${currentString.join("")}`;
+    time += 1;
+}, timeSpeed);
+})
+*/
+        
