c2cae436f9e93c352e044b251fef5792491d464d
destonkmuo <110706572+destonkmuo@users.noreply.github.com>
2022-10-09 20:46:11 -0500

    Cleaned Up Code and FIxed False Errors...

--- en-us/Home/JavaScript/LowNavWidth.js
+++ en-us/Home/JavaScript/LowNavWidth.js
@@ -3,25 +3,28 @@ var button;
 var dropDown;
 var width, height;
 
-setTimeout(function(){
+setTimeout(function() {
     navList = document.getElementsByClassName('nav-list-item');
     button = document.getElementById('low-nav-width-btn');
     dropDown = document.getElementsByClassName('low-nav-width-drop-down');
-},1500);
+}, 1500);
 
 
-    window.onresize = window.onload = function() {    
+window.onresize = window.onload = function() {
+    try {
         width = window.innerWidth;
         height = window.innerHeight;
-        if(width < 1255) {
+        if (width < 1255) {
             var x;
-            if(width < 500) { x = 0 } else { 
+            if (width < 500) {
+                x = 0
+            } else {
                 x = 1;
                 navList[0].style.display = "block";
             }
             for (let i = x; i < navList.length - 1; i++) {
-            navList[i].style.display = "none";
-            button.style.display = "block";
+                navList[i].style.display = "none";
+                button.style.display = "block";
             }
         } else {
             for (let i = 0; i < navList.length - 1; i++) {
@@ -29,8 +32,12 @@ setTimeout(function(){
                 button.style.display = "none";
             }
         }
-    }
-    
+    } catch (error) {}
+}
+
+try {
     button.onclick = function() {
+
         dropDown[0].style.visibility = "visible";
     }
+} catch (error) {}
--- en-us/domStart.js
+++ en-us/domStart.js
@@ -1,95 +1,103 @@
 window.addEventListener('load', function() {
-var nav = document.getElementById('nav');
+    var nav = document.getElementById('nav');
 
 
-var navList = document.createElement('ul');
-navList.className = "nav-list";
-document.body.appendChild(navList);
-nav.appendChild(navList);
+    var navList = document.createElement('ul');
+    navList.className = "nav-list";
+    document.body.appendChild(navList);
+    nav.appendChild(navList);
 
-/////////////////////////////////////
+    /////////////////////////////////////
 
-var navListItem = document.createElement('li');
-navListItem.className = "nav-list-item";
+    var navListItem = document.createElement('li');
+    navListItem.className = "nav-list-item";
 
-var href = document.createElement('a');
-href.href = "../Home/";
+    var href = document.createElement('a');
+    href.href = "../Home/";
 
-var CMLogo = document.createElement("img");
-CMLogo.src = "Static/CodersMaximsLogo.png";
-CMLogo.id = "HeaderLogo";
+    var CMLogo = document.createElement("img");
+    CMLogo.src = "Static/CodersMaximsLogo.png";
+    CMLogo.id = "HeaderLogo";
 
-document.body.appendChild(navListItem);
-document.body.appendChild(href);
-document.body.appendChild(CMLogo);
+    document.body.appendChild(navListItem);
+    document.body.appendChild(href);
+    document.body.appendChild(CMLogo);
 
-href.appendChild(CMLogo);
-navListItem.appendChild(href);
-navList.appendChild(navListItem);
+    href.appendChild(CMLogo);
+    navListItem.appendChild(href);
+    navList.appendChild(navListItem);
 
-///////////////////////////////////////////////
-var navInfo = [["Home","../Home/"],["Modules","#"],["Overview","#"],["Resources","#"],["Projects","#"],["References","#"],["Contact","#"]];
-for(i = 0; i < navInfo.length; i++) {
-var navListItem = document.createElement('li');
-navListItem.className = "nav-list-item";
+    ///////////////////////////////////////////////
+    var navInfo = [
+        ["Home", "../Home/"],
+        ["Modules", "#"],
+        ["Overview", "#"],
+        ["Resources", "#"],
+        ["Projects", "#"],
+        ["References", "#"],
+        ["Contact", "#"]
+    ];
+    for (i = 0; i < navInfo.length; i++) {
+        var navListItem = document.createElement('li');
+        navListItem.className = "nav-list-item";
 
-var href = document.createElement('a');
-href.href = navInfo[i][1];
+        var href = document.createElement('a');
+        href.href = navInfo[i][1];
 
-href.innerHTML = navInfo[i][0];
+        href.innerHTML = navInfo[i][0];
 
-document.body.appendChild(navListItem);
-document.body.appendChild(href);
+        document.body.appendChild(navListItem);
+        document.body.appendChild(href);
 
-navListItem.appendChild(href);
-navList.appendChild(navListItem);
-}
+        navListItem.appendChild(href);
+        navList.appendChild(navListItem);
+    }
 
-///////////////////////////////////////////////
+    ///////////////////////////////////////////////
 
-var navListItem = document.createElement('li');
-navListItem.className = "nav-list-item";
+    var navListItem = document.createElement('li');
+    navListItem.className = "nav-list-item";
 
-var searchBoxDiv = document.createElement('div');
-searchBoxDiv.className = "search-box";
-searchBoxDiv.id = "search-button";
+    var searchBoxDiv = document.createElement('div');
+    searchBoxDiv.className = "search-box";
+    searchBoxDiv.id = "search-button";
 
-var searchButton = document.createElement('button');
-searchButton.className = "btn-search";
+    var searchButton = document.createElement('button');
+    searchButton.className = "btn-search";
 
-var searchButtonImg = document.createElement('img');
-searchButtonImg.src = "Static/SearchLogo.png";
-searchButtonImg.id = "SearchLogo";
+    var searchButtonImg = document.createElement('img');
+    searchButtonImg.src = "Static/SearchLogo.png";
+    searchButtonImg.id = "SearchLogo";
 
-var faSearch = document.createElement('i');
-faSearch.className = "fa fa-search";
+    var faSearch = document.createElement('i');
+    faSearch.className = "fa fa-search";
 
-var input = document.createElement('input');
-input.type = "text";
-input.name = "";
-input.id = "input-search";
-input.setAttribute("onkeyup", "Search()");
-input.placeholder= "Search...";
+    var input = document.createElement('input');
+    input.type = "text";
+    input.name = "";
+    input.id = "input-search";
+    input.setAttribute("onkeyup", "Search()");
+    input.placeholder = "Search...";
 
-var resultsContent = document.createElement('ul');
-resultsContent.id = "results-content";
+    var resultsContent = document.createElement('ul');
+    resultsContent.id = "results-content";
 
-document.body.appendChild(navListItem);
-document.body.appendChild(searchBoxDiv);
-document.body.appendChild(searchButton);
-document.body.appendChild(searchButtonImg);
-document.body.appendChild(faSearch);
-document.body.appendChild(input);
-document.body.appendChild(resultsContent);
+    document.body.appendChild(navListItem);
+    document.body.appendChild(searchBoxDiv);
+    document.body.appendChild(searchButton);
+    document.body.appendChild(searchButtonImg);
+    document.body.appendChild(faSearch);
+    document.body.appendChild(input);
+    document.body.appendChild(resultsContent);
 
-searchBoxDiv.appendChild(searchButton);
-searchBoxDiv.appendChild(faSearch);
-searchBoxDiv.appendChild(input);
-searchBoxDiv.appendChild(resultsContent);
-searchButton.appendChild(searchButtonImg);
+    searchBoxDiv.appendChild(searchButton);
+    searchBoxDiv.appendChild(faSearch);
+    searchBoxDiv.appendChild(input);
+    searchBoxDiv.appendChild(resultsContent);
+    searchButton.appendChild(searchButtonImg);
 
-navListItem.appendChild(searchBoxDiv);
-navList.appendChild(navListItem);
-///////////////////////////////////////////////
+    navListItem.appendChild(searchBoxDiv);
+    navList.appendChild(navListItem);
+    ///////////////////////////////////////////////
 
 })
