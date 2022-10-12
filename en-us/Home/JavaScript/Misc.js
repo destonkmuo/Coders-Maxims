e53bd01795b248cf7cc73e2573bbaafe1ef93e2d
destonkmuo <110706572+destonkmuo@users.noreply.github.com>
2022-10-12 03:31:10 -0500

    Low Nav Width Width FIx

--- en-us/Home/JavaScript/Misc.js
+++ en-us/Home/JavaScript/Misc.js
@@ -1,12 +1,23 @@
 var navVisibilityBar;
+var philosophicHelper;
 
 setTimeout(function() {
     navVisibilityBar = document.getElementById('nav-visibility-bar');
+    philosophicHelper = document.getElementById('philosophic-helper');
 }, 100);
+
+var USERSCROLLHEIGHT;
 
 window.addEventListener("scroll", (event) => {
     var userScrollHeight = Math.floor(this.scrollY);
+    USERSCROLLHEIGHT = userScrollHeight;
     userScrollHeight > 10 ? navVisibilityBar.style.opacity = .85 : navVisibilityBar.style.opacity = 0;
-});
+    userScrollHeight > 400 ? philosophicHelper.style.opacity = .85 : philosophicHelper.style.opacity = 0; 
+})
 
-// Nav Visibility 
+var userScrollHeightAtLastInterval;
+var interval = setInterval(function(){
+userScrollHeightAtLastInterval = USERSCROLLHEIGHT;
+if(userScrollHeightAtLastInterval + 25 > USERSCROLLHEIGHT || userScrollHeightAtLastInterval - 25 < USERSCROLLHEIGHT) {
+    philosophicHelper.style.opacity = 0;
+}}, 5000);
