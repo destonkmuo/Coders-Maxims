c23994e426261e906dfe69fecb4beb09726a09e2
destonkmuo <110706572+destonkmuo@users.noreply.github.com>
2022-10-29 14:44:55 -0500

    SF

--- JavaScript/Search.js
+++ JavaScript/Search.js
@@ -25,14 +25,13 @@ function Search() {
             for (let modulesIndex = 0; modulesIndex < modules.length; modulesIndex++) {
                 if (results.includes(modules[modulesIndex].href || input == " ")) {
                     if(devLog) console.error(`Results Contains Space or Already Contains User Input... Resetting: ${results}`);
-                    resetResults();
-                }  else if((modules[modulesIndex].name.toLowerCase().includes(input)) && input.length > 0 && results.size < 6) {
+                }  else if((modules[modulesIndex].name.toLowerCase().includes(input)) && input.length > 0 && results.length < 6) {
                     if(devLog) console.log(`Found ${modules[modulesIndex].name}... Appending Potential href`);
                     results.push([modules[modulesIndex].href, modules[modulesIndex].name]);
                 }
             }
-            
-            for(let i = 0; i < results.size; i++) {
+
+            for(let i = 0; i < results.length; i++) {
                 var searchResult = document.createElement("a");
                 searchResult.innerHTML = `🔍︎ ${results[i][1]}`;
                 searchResult.href = results[i][0];
@@ -53,8 +52,8 @@ function Search() {
                 }
             }
 
-            results.size > 0 ? visibility(true) : visibility(false);
-            searchButton.onmousedown = function() { if (results.size > 0) { visibility(true) }}
+            results.length > 0 ? visibility(true) : visibility(false);
+            searchButton.onmousedown = function() { if (results.length > 0) { visibility(true) }}
             searchButton.onmouseleave = function() { visibility(false) }
         })
     resetResults();
