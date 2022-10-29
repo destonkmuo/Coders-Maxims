ac58e77e65d850db52c0617a001dc4f47ac7e659
destonkmuo <110706572+destonkmuo@users.noreply.github.com>
2022-10-29 14:48:35 -0500

    SF

--- JavaScript/Search.js
+++ JavaScript/Search.js
@@ -13,7 +13,7 @@ function Search() {
 
     document.getElementById("input-search").addEventListener("keyup", function(event) {
         event.preventDefault();
-        if (event.key === 'Enter' && input != " ") {
+        if (event.key === 'Enter' && input != "") {
             window.location.href = `/s_dir/GlobalSearch/?search=${input}`;
         }
     });
@@ -23,21 +23,12 @@ function Search() {
         .then(modules => {
 
             for (let modulesIndex = 0; modulesIndex < modules.length; modulesIndex++) {
-                if (results.includes(modules[modulesIndex].href || input == " ")) {
+                if (results.includes(modules[modulesIndex].href || input == "")) {
                     if(devLog) console.error(`Results Contains Space or Already Contains User Input... Resetting: ${results}`);
                 }  else if((modules[modulesIndex].name.toLowerCase().includes(input)) && input.length > 0 && results.length < 6) {
                     if(devLog) console.log(`Found ${modules[modulesIndex].name}... Appending Potential href`);
                     results.push([modules[modulesIndex].href, modules[modulesIndex].name]);
                 }
-            }
-
-            for(let i = 0; i < results.length; i++) {
-                var searchResult = document.createElement("a");
-                searchResult.innerHTML = `🔍︎ ${results[i][1]}`;
-                searchResult.href = results[i][0];
-                searchResult.className = "results-lists";
-                document.body.appendChild(searchResult);
-                searchResultsContent.appendChild(searchResult);
             }
 
             function visibility(boolean) {
@@ -56,5 +47,14 @@ function Search() {
             searchButton.onmousedown = function() { if (results.length > 0) { visibility(true) }}
             searchButton.onmouseleave = function() { visibility(false) }
         })
+
+        for(let i = 0; i < results.length; i++) {
+            var searchResult = document.createElement("a");
+            searchResult.innerHTML = `🔍︎ ${results[i][1]}`;
+            searchResult.href = results[i][0];
+            searchResult.className = "results-lists";
+            document.body.appendChild(searchResult);
+            searchResultsContent.appendChild(searchResult);
+        }
     resetResults();
 }
